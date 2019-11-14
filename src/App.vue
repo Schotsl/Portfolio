62e3c5aefb094221f3f0f33254798129036840a7
Sjors van Holst <sjorsvanholst@gmail.com>
2019-11-14 15:17:15 +0100

    Added global styling

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,14 @@
   html, body, #app {
     width: 100%;
     margin: 0px;
+    
+    font-weight: 300;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3, h4, h5 {
+    font-weight: 300;
+    margin: 0.27em 0;
   }
 
   html {
