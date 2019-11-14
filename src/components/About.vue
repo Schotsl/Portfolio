015496f33cc036e998f977be6581d1478128903f
Sjors van Holst <sjorsvanholst@gmail.com>
2019-11-14 15:18:05 +0100

    Styled list

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,63 +1,76 @@
 <template>
 <section id="about">
-  <img alt="Vue logo" class="self" src="./../assets/About/Sjors van Holst image.png">
+  <img alt="Vue logo" src="./../assets/About/Sjors van Holst image.png">
 
   <div>
     <h1>About me</h1>
-    <p>
-      I’m Sjors van Holst, a passionate programmer with a focus on web design and experience with PHP, JS, HTML, CSS, C#, Vue, WordPress and AJAX. I’ve got a calm and friendly attitude with a positive approach, I grew up and live in the Netherlands. I finished primary school and continued to high school, where I was introduced to my first programming language: Processing!
-    </p>
+    <p>I’m Sjors van Holst, a passionate programmer with a focus on web design and experience with PHP, JS, HTML, CSS, C#, Vue, WordPress and AJAX. I’ve got a calm and friendly attitude with a positive approach, I grew up and live in the Netherlands. I finished primary school and continued to high school, where I was introduced to my first programming language: Processing!</p>
+    <p>After spending about a year learning al the ins and outs of processing I was ready to move on in a different direction. I fast tracked my study "Applicatie- en Mediaontwikkelaar" since I already had experience programmin. I continued on to study "Informatica" at InHolland Haarlem. I was born on March 22, which makes me 19 years old. If your interested you can download a copy of my CV by clicking here.</p>
 
-    <p>
-      After spending about a year learning al the ins and outs of processing I was ready to move on in a different direction. I fast tracked my study "Applicatie- en Mediaontwikkelaar" since I already had experience programmin. I continued on to study "Informatica" at InHolland Haarlem. I was born on March 22, which makes me 19 years old. If your interested you can download a copy of my CV by clicking here.
-    </p>
+    <h3>Education</h3>
 
-    <h2>Education</h2>
-    <h3>Kajmunk College (2013 - 2017):</h3>
-    <ul>
-      <li>Physics</li>
-      <li>English</li>
-      <li>Math</li>
-      <li>Art</li>
-    </ul>
-    <h3>ROC van Amsterdam (2017 - current):</h3>
-    <ul>
-      <li>JavaScript</li>
-      <li>HTML/CSS</li>
-      <li>Ajax</li>
-      <li>PHP</li>
-      <li>C#</li>
-    </ul>
+    <section class="sub">
+      <h4>Kajmunk College (2013 - 2017):</h4>
+      <ul>
+        <li>Physics</li>
+        <li>English</li>
+        <li>Math</li>
+        <li>Art</li>
+      </ul>
+    </section>
 
-    <h2>Work</h2>
-    <h3>Student aan Huis (8 months):</h3>
-    <ul>
-      <li>IT Support at home</li>
-    </ul>
-    <h3>Get Interactive (5 months):</h3>
-    <ul>
-      <li>Internship as a web developer</li>
-    </ul>
-    <h3>Get Interactive (4 months):</h3>
-    <ul>
-      <li>Part time software developer</li>
-    </ul>
+    <section class="sub">
+      <h4>ROC van Amsterdam (2017 - current):</h4>
+      <ul>
+        <li>JavaScript</li>
+        <li>HTML/CSS</li>
+        <li>Ajax</li>
+        <li>PHP</li>
+        <li>C#</li>
+      </ul>
+    </section>
 
-    <h2>Courses</h2>
-    <h3>Secured by design:</h3>
-    <ul>
-      <li>Ethical hacking</li>
-    </ul>
-    <h3>ROC Light license:</h3>
-    <ul>
-      <li>Aviation rules &amp; laws</li>
-    </ul>
+    <h3>Work</h3>
 
-    <h2>Competitions</h2>
-    <h3>Skills heroes (National championship):</h3>
-    <ul>
-      <li>Application developer</li>
-    </ul>
+    <section class="sub">
+      <h4>Student aan Huis:</h4>
+      <ul>
+        <li>Part time IT Support at home (8 months)</li>
+      </ul>
+    </section>
+
+    <section class="sub">
+    <h4>Get Interactive:</h4>
+      <ul>
+        <li>Full time Internship as a web developer <b>(8 months)</b></li>
+        <li>Part time software developer <b>(5 months)</b></li>
+      </ul>
+    </section>
+
+    <h3>Courses</h3>
+
+    <section class="sub">
+      <h4>Secured by design:</h4>
+      <ul>
+        <li>Ethical hacking</li>
+      </ul>
+    </section>
+
+    <section class="sub">
+      <h4>ROC Light license:</h4>
+      <ul>
+        <li>Aviation rules &amp; laws</li>
+      </ul>
+    </section>
+
+    <h3>Competitions</h3>
+
+    <section class="sub">
+      <h4>Skills heroes (National championship):</h4>
+      <ul>
+        <li>Application developer</li>
+      </ul>
+    </section>
   </div>
 </section>
 </template>
@@ -72,21 +85,37 @@
   #about {
     height: 100vh;
     width: 100vw;
+
     padding: 1%;
 
-    display: flex;
     box-sizing: border-box;
+    display: flex;
   }
 
   img {
+    object-fit: cover;
     height: 100%;
+    width: 30%;
   }
 
   div {
     margin-left: 1%;
   }
 
+  .sub {
+    margin: 0.27em 1em;
+  }
+
   ul {
+    padding: 0;
+    margin: 0 1em;
     list-style-type: none;
-}
+  }
+
+  h3 {
+    border-top: 1px solid rgba(0, 0, 0, 0.25);
+
+    padding-top: 0.25em;
+    margin-top: 0.75em;
+  }
 </style>
