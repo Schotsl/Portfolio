63e23b0a6d6a372392199e124add6cc395d5e403
Sjors van Holst <sjorsvanholst@gmail.com>
2020-02-03 19:05:20 +0100

    Added slide in image

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,12 @@
 <template>
 <section id="about">
-  <img alt="Vue logo" src="./../assets/About/Sjors van Holst image.png">
-
-  <div>
+  <u-animate-container class="u-animate-container" style="width: 39%; margin: 1%; margin-right: 0.5%;">
+    <u-animate class="u-animate" name="fadeInLeft" duration="1s" animateClass="fadeInUp">
+      <img alt="Vue logo" src="./../assets/About/Wouter de Bruijn & Sjors van Holst.png">
+    </u-animate>
+  </u-animate-container>
+  
+  <div style="width: 59%; margin: 1%; margin-left: 0.5%;">
     <h1>About me</h1>
     <p>I’m Sjors van Holst, a passionate programmer with a focus on web design and experience with PHP, JS, HTML, CSS, C#, Vue, WordPress and AJAX. I’ve got a calm and friendly attitude with a positive approach, I grew up and live in the Netherlands. I finished primary school and continued to high school, where I was introduced to my first programming language: Processing!</p>
     <p>After spending about a year learning al the ins and outs of processing I was ready to move on in a different direction. I fast tracked my study "Applicatie- en Mediaontwikkelaar" since I already had experience programmin. I continued on to study "Informatica" at InHolland Haarlem. I was born on March 22, which makes me 19 years old. If your interested you can download a copy of my CV by clicking here.</p>
@@ -76,30 +80,36 @@
 </template>
 
 <script>
-  export default {
-    name: 'About'
+import { UAnimateContainer, UAnimate } from 'vue-wow';
+
+export default {
+  name: 'About',
+
+  components: {
+    UAnimateContainer,
+    UAnimate
   }
+}
 </script>
 
 <style scoped>
-  #about {
-    height: 100vh;
-    width: 100vw;
+#about {
+  height: 100vh;
+  width: 100vw;
 
-    padding: 1%;
-
-    box-sizing: border-box;
-    display: flex;
-  }
+  box-sizing: border-box;
+  display: flex;
+}
 
   img {
-    object-fit: cover;
-    height: 100%;
-    width: 30%;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
   }
 
-  div {
-    margin-left: 1%;
+  .u-animate {
+    width: 100%;
+    height: 100%;
   }
 
   .sub {
@@ -118,4 +128,22 @@
     padding-top: 0.25em;
     margin-top: 0.75em;
   }
+
+  @keyframes fadeInUp {
+  0% {
+    transform: translate3d(-30%, 0, 0);
+    opacity: 0;
+  }
+
+  100% {
+    transform: none;
+    opacity: 1;
+  }
+}
+
+.fadeInUp {
+  opacity: 0;
+  animation-name: fadeInUp;
+  animation-timing-function: ease-out;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueWow from 'vue-wow';
+
+Vue.use(VueWow)
 
 Vue.config.productionTip = false
 
