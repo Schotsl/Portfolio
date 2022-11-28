13868c3fc9a51e6471c6861821457175abcfc120
Sjors van Holst <sjorsvanholst@gmail.com>
2022-11-28 20:54:22 +0100

    🐛 Fixed build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+import eslintPlugin from "vite-plugin-eslint";
+
+export default defineNuxtConfig({
+  target: "static",
+  vite: {
+    plugins: [eslintPlugin()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_imports.scss" as *;',
+        },
+      },
+    },
+  },
+});
