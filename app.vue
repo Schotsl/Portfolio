d3330bbb36e8282201690323abdca38a75727f26
Sjors van Holst <sjorsvanholst@gmail.com>
2022-12-22 19:34:57 +0100

    💄 Styled homepage

--- app.vue
+++ app.vue
@@ -1,15 +1,11 @@
 <script setup>
 useHead({
-  // link: [
-  //   {
-  //     rel: "stylesheet",
-  //     href: "https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap",
-  //   },
-  //   {
-  //     rel: "stylesheet",
-  //     href: "https://fonts.googleapis.com/css2?family=Aclonica&family=Open+Sans:wght@300;400;600;800&display=swap",
-  //   },
-  // ],
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800",
+    },
+  ],
   htmlAttrs: {
     lang: "nl",
   },
@@ -21,3 +17,53 @@ useHead({
     <NuxtPage />
   </main>
 </template>
+
+<style lang="scss">
+:root {
+  --background-1: #150485;
+  --background-2: #590995;
+  --background-3: #c62a88;
+}
+
+html,
+html > body,
+html > body > div,
+html > body > div > main {
+  width: 100%;
+  height: 100%;
+
+  color: #fff;
+  margin: 0;
+  padding: 0;
+
+  font-family: "Open Sans", sans-serif;
+}
+
+// Made with the help of https://www.gradient-animator.com
+
+main {
+  background: linear-gradient(
+    130deg,
+    var(--background-1),
+    var(--background-2),
+    var(--background-3)
+  );
+
+  animation: gradient 15s ease infinite;
+  background-size: 600% 600%;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 10%;
+  }
+
+  50% {
+    background-position: 100% 91%;
+  }
+
+  100% {
+    background-position: 0% 10%;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -36,29 +36,138 @@ onMounted(() => {
 <template>
   <form @submit.prevent="redirectUser">
     <fieldset>
-      <legend>For what are you looking?</legend>
+      <legend>I'm looking for a</legend>
 
-      <div>
+      <label for="developer">
         <input
           id="developer"
           v-model="developer"
           type="checkbox"
           name="developer"
         />
-        <label for="developer">Developer</label>
-      </div>
+        Developer
+      </label>
 
-      <div>
+      <label for="designer">
         <input
           id="designer"
           v-model="designer"
           type="checkbox"
           name="designer"
         />
-        <label for="designer">Designer</label>
-      </div>
+        Designer
+      </label>
     </fieldset>
 
     <button :disabled="!userSelected" type="submit">Show me your work</button>
   </form>
 </template>
+
+<style lang="scss" scoped>
+form {
+  padding: 4rem;
+}
+
+form > button {
+  font-size: 2rem;
+  margin: 2.5rem 0 0;
+}
+
+form > button {
+  color: #fff;
+  border: 0.15em solid #fff;
+  font-weight: 500;
+  border-radius: 5rem;
+  padding: 0.7rem 2rem 0.9rem;
+  background-color: Transparent;
+  background-repeat: no-repeat;
+
+  cursor: pointer;
+  overflow: hidden;
+  outline: none;
+
+  transition: all 0.15s ease-in-out;
+}
+
+form > button:hover {
+  background: white;
+  color: black;
+  mix-blend-mode: screen;
+}
+
+form > fieldset {
+  border: none;
+  padding: 0;
+  margin: 0;
+}
+
+form > fieldset > legend {
+  font-size: 3rem;
+  margin: 0 0 2rem;
+}
+
+// Made with the help of https://moderncss.dev/pure-css-custom-checkbox-style
+
+label {
+  font-size: 2rem;
+  font-weight: 600;
+  line-height: 0.9;
+  display: grid;
+  grid-template-columns: 1em auto;
+  gap: 1.5rem;
+}
+
+label + label {
+  margin-top: 1.5rem;
+}
+
+input[type="checkbox"] {
+  /* Add if not using autoprefixer */
+  -webkit-appearance: none;
+  /* Remove most all native input styles */
+  appearance: none;
+  /* For iOS < 15 */
+  background-color: var(--form-background);
+  /* Not removed via appearance */
+  margin: 0;
+
+  font: inherit;
+  color: currentColor;
+  width: 1.15em;
+  height: 1.15em;
+  border: 0.15em solid currentColor;
+  border-radius: 0.15em;
+  transform: translateY(-0.075em);
+
+  display: grid;
+  place-content: center;
+}
+
+input[type="checkbox"]::before {
+  content: "";
+  width: 0.55em;
+  height: 0.55em;
+  clip-path: polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%);
+  transform: scale(0);
+  transform-origin: bottom left;
+  box-shadow: inset 1em 1em #fff;
+  /* Windows High Contrast Mode */
+  background-color: CanvasText;
+}
+
+input[type="checkbox"]:checked::before {
+  transform: scale(1);
+}
+
+input[type="checkbox"]:focus {
+  outline: 0.1em solid rgba(255, 255, 255, 0.5);
+  outline-offset: max(2px, 0.15em);
+}
+
+input[type="checkbox"]:disabled {
+  --form-control-color: var(--form-control-disabled);
+
+  color: var(--form-control-disabled);
+  cursor: not-allowed;
+}
+</style>
