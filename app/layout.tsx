9a6ac02725460bd6f7288a4487c6671f8d07e21f
Sjors van Holst <sjorsvanholst@gmail.com>
2024-04-22 21:20:29 +0200

    ⬆️ Upgraded dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,7 @@ const alt = "Een foto van de Toolenburgerplas in Hoofddorp.";
 const url = "https://toolenburgerplas.nl/banner-1200x630.png";
 
 const title = "Sjors van Holst";
-const description =
-  "Ik ben Sjors van Holst";
+const description = "Ik ben Sjors van Holst";
 
 export const metadata: Metadata = {
   title,
@@ -44,9 +43,7 @@ export const metadata: Metadata = {
     ],
   },
 
-  authors: [
-    { name: "Sjors van Holst", url: "https://sjorsvanholst" },
-  ],
+  authors: [{ name: "Sjors van Holst", url: "https://sjorsvanholst" }],
 };
 
 const openSans = Open_Sans({
@@ -60,47 +57,41 @@ export default async function RootLayout({
   children: ReactNode;
 }) {
   return (
-      <PlausibleProvider
-        domain="sjorsvanholst.nl"
-        enabled={true}
-        selfHosted={true}
-        customDomain="https://plausible.hedium.nl"
-      >
-        <html lang="en" className={openSans.className}>
-          <head>
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/apple-touch-icon.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="32x32"
-              href="/favicon-32x32.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="16x16"
-              href="/favicon-16x16.png"
-            />
+    <PlausibleProvider
+      domain="sjorsvanholst.nl"
+      enabled={true}
+      selfHosted={true}
+      customDomain="https://plausible.hedium.nl"
+    >
+      <html lang="en" className={openSans.className}>
+        <head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
 
-            <link rel="manifest" href="/site.webmanifest" />
-            <link
-              rel="mask-icon"
-              href="/safari-pinned-tab.svg"
-              color="#5bbad5"
-            />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
 
-            <meta name="msapplication-TileColor" content="#ffffff" />
-            <meta name="theme-color" content="#ffffff" />
-          </head>
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff" />
+        </head>
 
-          <body>
-            {children}
-          </body>
-        </html>
-      </PlausibleProvider>
+        <body>{children}</body>
+      </html>
+    </PlausibleProvider>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,30 +1,32 @@
 /** @type {import('next').NextConfig} */
 
-const { withPlausibleProxy } = require('next-plausible')
+const { withPlausibleProxy } = require("next-plausible");
 const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withPlausibleProxy()(withSentryConfig(
-  {
-    async rewrites() {
-      return [
-        {
-          source: "/:path*",
-          destination: "/",
-        },
-      ];
+module.exports = withPlausibleProxy()(
+  withSentryConfig(
+    {
+      async rewrites() {
+        return [
+          {
+            source: "/:path*",
+            destination: "/",
+          },
+        ];
+      },
     },
-  },
-  {
-    org: "sjors-van-holst-0c97a3b77",
-    silent: true,
-    release: "1.0.0",
-    project: "toolenburgerplas",
-  },
-  {
-    disableLogger: true,
-    hideSourceMaps: true,
-    transpileClientSDK: true,
-    widenClientFileUpload: true,
-    automaticVercelMonitors: true,
-  },
-));
+    {
+      org: "sjors-van-holst-0c97a3b77",
+      silent: true,
+      release: "1.0.0",
+      project: "toolenburgerplas",
+    },
+    {
+      disableLogger: true,
+      hideSourceMaps: true,
+      transpileClientSDK: true,
+      widenClientFileUpload: true,
+      automaticVercelMonitors: true,
+    },
+  ),
+);
