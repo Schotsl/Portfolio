0490517bf2214daa6698433d261350cf9bd5be5a
Sjors van Holst <sjorsvanholst@gmail.com>
2024-04-23 10:33:20 +0200

    ♻️ Splitted index.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,32 @@
-import { auth } from "../../../index";
-import { NextRequest } from "next/server";
+import { randomBytes } from "crypto";
+import { AuthorizationCode } from "simple-oauth2";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-  return auth(request);
+export const oauthConfig = {
+  client: {
+    id: process.env.OAUTH_CLIENT_ID!,
+    secret: process.env.OAUTH_CLIENT_SECRET!,
+  },
+  auth: {
+    tokenHost: "https://github.com",
+    tokenPath: "/login/oauth/access_token",
+    authorizePath: "/login/oauth/authorize",
+  },
+};
+
+/** An endpoint to start an OAuth2 authentication */
+export async function GET(req: NextRequest) {
+  const host = req.headers.get("host");
+  const code = new AuthorizationCode(oauthConfig);
+
+  const stateBytes = randomBytes(6);
+  const stateHex = stateBytes.toString("hex");
+
+  const url = code.authorizeURL({
+    redirect_uri: `https://${host}/api/callback`,
+    scope: `repo,user`,
+    state: stateHex,
+  });
+
+  return NextResponse.redirect(url);
 }
--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -1,6 +1,35 @@
-import { callback } from "../../../index";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { AuthorizationCode } from "simple-oauth2";
 
-export async function GET(request: NextRequest) {
-  return callback(request);
+export const oauthConfig = {
+  client: {
+    id: process.env.OAUTH_CLIENT_ID!,
+    secret: process.env.OAUTH_CLIENT_SECRET!,
+  },
+  auth: {
+    tokenHost: "https://github.com",
+    tokenPath: "/login/oauth/access_token",
+    authorizePath: "/login/oauth/authorize",
+  },
+};
+
+/** An endpoint to finish an OAuth2 authentication */
+export async function GET(req: NextRequest) {
+  const host = req.headers.get("host");
+  const code = req.nextUrl.searchParams.get("code")!;
+
+  const authCode = new AuthorizationCode(oauthConfig);
+  const authToken = await authCode.getToken({
+    code,
+    redirect_uri: `https://${host}/api/callback`,
+  })!;
+
+  const token = authToken.token.access_token;
+  const parsed = JSON.stringify({ token, provider: "github" });
+  const content = `<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Authorizing...</title></head><body><p id="message"></p><script>function sendMessage(message){document.getElementById("message").innerText=message;document.title=message}function receiveMessage(message){window.opener.postMessage('authorization:github:success:${parsed}',message.origin);window.removeEventListener("message",receiveMessage,false);sendMessage("Authorized, closing...");}sendMessage("Authorizing...");window.addEventListener("message",receiveMessage,false);window.opener.postMessage("authorizing:github","*");</script></body></html>`;
+
+  return new NextResponse(content, {
+    status: 200,
+    headers: { "Content-Type": "text/html" },
+  });
 }
