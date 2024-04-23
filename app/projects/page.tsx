240de554082ec5c44e8d6804f6d1a65bb7a54a94
Sjors van Holst <sjorsvanholst@gmail.com>
2024-04-23 21:36:36 +0200

    ✨ Added Image component

---
+++ app/projects/page.tsx
@@ -0,0 +1,29 @@
+import fs from "fs";
+import Image from "@/component/Image";
+
+const projects = "./content/projects";
+const projectsNames = fs.readdirSync(projects);
+const projectsObjects = projectsNames.map((projectName) => {
+  const projectPath = `${projects}/${projectName}`;
+  const projectObject = fs.readFileSync(projectPath, "utf8");
+
+  return JSON.parse(projectObject);
+});
+
+export default async function Page() {
+  return (
+    <main>
+      <h1>Test</h1>
+      <ul>
+        {projectsObjects.map((project) => (
+          <li key={project.id}>
+            <h2>{project.title}</h2>
+
+            <Image src={project.image} alt={project.title} />
+            <p>{project.description}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
---
+++ component/Image/index.tsx
@@ -0,0 +1,60 @@
+import styles from "./Image.module.scss";
+
+import fs from "fs";
+import sizeOf from "buffer-image-size";
+import NextImage from "next/image";
+
+type ImageProps = {
+  src: string;
+  alt: string;
+  sizes?: string;
+  quality?: number;
+};
+
+const toBase64 = (value: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(value).toString("base64")
+    : window.btoa(value);
+
+export default async function Image({ src, alt, sizes, quality = 75 }: ImageProps) {
+  const image = fs.readFileSync(`${process.cwd()}/public/${src}`);
+
+  const { width, height } = sizeOf(image);
+
+  const baseUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : process.env.NEXT_PUBLIC_DOMAIN;
+
+  const imageUrl = `${baseUrl}/_next/image?url=${src}&w=16&q=75`;
+  const imageResponse = await fetch(imageUrl);
+
+  const blurBuffer = await imageResponse.arrayBuffer();
+  const blurBase64 = Buffer.from(blurBuffer).toString("base64");
+  const blurSVG = `
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 5'>
+      <filter id="blur" x="0%" y="0%" width="100%" height="100%">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="0.5"/>
+          <feComponentTransfer>
+          <feFuncA type="discrete" tableValues="1 1"/>
+        </feComponentTransfer>
+      </filter>
+
+      <image preserveAspectRatio='none' filter='url(#blur)' x='0' y='0' height='100%' width='100%' href='data:image/avif;base64,${blurBase64}' />
+    </svg>
+  `;
+
+  return (
+    <NextImage
+      src={src}
+      alt={alt}
+      sizes={sizes}
+      width={width}
+      height={height}
+      quality={quality}
+      className={styles.image}
+      placeholder="blur"
+      blurDataURL={`data:image/svg+xml;base64,${toBase64(blurSVG)}`}
+    />
+  );
+}
