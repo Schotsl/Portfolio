c0ec10f19107e3e7b6b152b6431ab5fc742d464d
Sjors van Holst <sjorsvanholst@gmail.com>
2024-04-28 21:23:29 +0200

    💩 Implemented carousel

--- component/Carousel/index.tsx
+++ component/Carousel/index.tsx
@@ -5,13 +5,15 @@ import styles from "./Carousel.module.scss";
 import Image from "@/component/Image";
 
 import { Project } from "@/types";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type CarouselProps = {
   projects: Project[];
 };
 
 export default function Carousel({ projects }: CarouselProps) {
+  const [slide, setSlide] = useState(0);
+
   const carousel = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
@@ -27,10 +29,27 @@ export default function Carousel({ projects }: CarouselProps) {
     }
   }, []);
 
+  function onScroll() {
+    if (carousel.current) {
+      const scrollLeft = carousel.current.scrollLeft;
+      const scrollItem = carousel.current.scrollWidth / projects.length;
+      const scrollIndex = Math.round(scrollLeft / scrollItem);
+      
+      setSlide(scrollIndex);
+    }
+  }
+
   return (
-    <ul className={styles.carousel} ref={carousel}>
+    <ul className={styles.carousel} ref={carousel} onScroll={onScroll}>
       {projects.map((project, index) => (
-        <li className={styles.carousel__item} key={index}>
+        <li
+          key={index}
+          className={
+            index === slide
+              ? `${styles.carousel__item} ${styles["carousel__item--active"]}`
+              : `${styles.carousel__item}`
+          }
+        >
           <Image image={project.image} sizes={"46rem"} />
         </li>
       ))}
--- component/Image/index.tsx
+++ component/Image/index.tsx
@@ -27,7 +27,7 @@ export default function Image({
   }
 
   return (
-    <div className={styles.image}>
+    <div className={styles.image} style={{ aspectRatio: `${width}/${height}` }}>
       <NextImage
         src={src}
         alt={alt}
@@ -41,24 +41,16 @@ export default function Image({
 
       {loading && (
         <div className={styles.image__overlay}>
-          {/* eslint-disable-next-line */}
-          <img
-            src={`data:image/svg+xml;base64,${blur}`}
-            alt={alt}
-            className={styles.image__overlay__blur}
-          />
-
-          <div className={styles.image__overlay__loader}>
-            <div className={styles.image__overlay__loader__dot}></div>
-            <div className={styles.image__overlay__loader__dot}></div>
-            <div className={styles.image__overlay__loader__dot}></div>
-            <div className={styles.image__overlay__loader__dot}></div>
-            <div className={styles.image__overlay__loader__dot}></div>
-            <div className={styles.image__overlay__loader__dot}></div>
-            <div className={styles.image__overlay__loader__dot}></div>
-            <div className={styles.image__overlay__loader__dot}></div>
-            <div className={styles.image__overlay__loader__dot}></div>
+          <div className={styles.image__overlay__image}>
+            {/* eslint-disable-next-line */}
+            <img
+              src={`data:image/svg+xml;base64,${blur}`}
+              alt={alt}
+              className={styles.image__overlay__image__blur}
+            />
           </div>
+
+          <div className={styles.image__overlay__skeleton}></div>
         </div>
       )}
     </div>
