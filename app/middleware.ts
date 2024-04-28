7bfac1b50552003698805d60352bf1ace50f0849
Sjors van Holst <sjorsvanholst@gmail.com>
2024-04-28 14:14:29 +0200

    🐛 Testing

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,23 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server'
  
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
-  ],
-}
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - api (API routes)
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico (favicon file)
+//      */
+//     {
+//       source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+//       missing: [
+//         { type: 'header', key: 'next-router-prefetch' },
+//         { type: 'header', key: 'purpose', value: 'prefetch' },
+//       ],
+//     },
+//   ],
+// }
 
 export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
@@ -40,7 +40,6 @@ export function middleware(request: NextRequest) {
  
   const requestHeaders = new Headers(request.headers)
   requestHeaders.set('x-nonce', nonce)
- 
   requestHeaders.set(
     'Content-Security-Policy',
     contentSecurityPolicyHeaderValue
