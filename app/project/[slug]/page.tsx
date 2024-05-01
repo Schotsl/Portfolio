71d2681cfb7666ecfff8acd7761e501f9a0ebeb2
Sjors van Holst <sjorsvanholst@gmail.com>
2024-05-01 20:38:39 +0200

    🐛 Added object

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -5,13 +5,18 @@ export const dynamicParams = false;
 
 export async function generateStaticParams() {
   const projects = await getCollection<Project>("project");
-  return projects;
+  const projectsSlug = projects.map((project) => project.slug);
+
+  return projectsSlug;
 }
 
-export default function Page({ params }: { params: Project }) {
+export default async function Page({ params }: { params: Project }) {
+  const projects = await getCollection<Project>("project");
+  const project = projects.find((project) => project.slug === params.slug);
+
   return (
     <div>
-      <h1>Project {JSON.stringify(params)}</h1>
+      <h1>Project {JSON.stringify(project)}</h1>
     </div>
   );
 }
