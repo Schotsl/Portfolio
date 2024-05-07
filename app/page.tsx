cd7b7bc50a55902ab14097f1188fc97ebd4310a6
Sjors van Holst <sjorsvanholst@gmail.com>
2024-05-07 19:32:54 +0200

    ♻️ Changed interval value

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,9 @@ import banner from "@/public/content/shared/banner.json";
 import content from "@/public/content/pages/home/index.json";
 import supabase from "@/utils/supabase";
 
-export const revalidate = 1;
+export const revalidate = 3600;
 
 async function loadCount() {
-  console.log("Loading count");
-
   const { data, error } = await supabase
     .from("statistics")
     .select("pageviews")
