d1dfcb2ae2105135f942384de1ee9617a4f7d491
Sjors van Holst <sjorsvanholst@gmail.com>
2024-05-07 12:36:21 +0200

    🔧 Updating CMS config

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -1,24 +1,32 @@
-import { getCollection } from "@/helper";
+import styles from "./page.module.scss";
+
+import { getCollection, getImage } from "@/helper";
 import { Project } from "@/types";
 
-export const dynamicParams = false;
+import Header from "@/component/Header";
 
-export async function generateStaticParams() {
-  const projects = await getCollection<Project>("project");
-  const projectsSlug = projects.map((project) => ({
-    slug: project.slug,
-  }));
+// export const dynamicParams = false;
 
-  return projectsSlug;
-}
+// export async function generateStaticParams() {
+//   const projects = await getCollection<Project>("project");
+//   const projectsSlug = projects.map((project) => ({
+//     slug: project.slug,
+//   }));
+
+//   return projectsSlug;
+// }
 
 export default async function Page({ params }: { params: { slug: string } }) {
+  const banner = await getImage("/images/banner.png", "Me");
+
   const projects = await getCollection<Project>("project");
   const project = projects.find((project) => project.slug === params.slug);
 
   return (
-    <div>
+    <main className={styles.main}>
+      <Header banner={banner} sentences={["Looking for this page"]} />
+
       <h1>Project {JSON.stringify(project)}</h1>
-    </div>
+    </main>
   );
 }
